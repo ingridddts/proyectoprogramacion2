6643265126ba177fa62a3a77ab38a7c0406286af
Ingrid <131918653+ingridddts@users.noreply.github.com>
2023-06-26 18:13:05 -0400

    Update Drawable.java
    documentacion

--- PizarraFinal/src/Dibujos/Drawable.java
+++ PizarraFinal/src/Dibujos/Drawable.java
@@ -3,38 +3,39 @@ package Dibujos;
 import java.awt.Color;
 import java.awt.Graphics;
 
-
+/**
+* Dibujar en la pizarra: arrastrar entidades y flechas y realizar acciones con el mouse
+*/
 public interface Drawable {
 
     /**
-     * Paint to the screen
+     * Dibujar en la pizarra
      * @param g
      */
     public void paint(Graphics g);
 
     /**
-     * Update with a new position. Called when the mouse is dragged
-     * @param x
-     * @param y
+     * Actualización con una nueva posición cuando se arrastra el mouse
+     * @param x posicion eje x
+     * @param y posicion eje y
      */
     public void update_from_pos(int x, int y);
     
     /**
-     * Set the origin. Called when the mouse is clicked
-     * @param x
-     * @param y
+     * Establecer origen al hacer click
+     * @param x posicion eje x 
+     * @param y posicion eje y
      */
     public void set_origin(int x, int y);
 
     /**
-     * Setter for color
-     * @param color The new color
+     * escoger el color
+     * @param color contiene el nuevo color
      */
     public void set_color(Color color);
-
     /**
-     * Getter for color
-     * @return The color
+     * obtiene el color
+     * @return color seleccionado
      */
     public Color get_color();
     }

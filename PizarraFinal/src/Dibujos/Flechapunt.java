efdd7b2db5355e9dbad117ca4a85a3d2f484468e
Ingrid <131918653+ingridddts@users.noreply.github.com>
2023-06-26 18:29:55 -0400

    Update Flechapunt.java
    documentacion

--- PizarraFinal/src/Dibujos/Flechapunt.java
+++ PizarraFinal/src/Dibujos/Flechapunt.java
@@ -1,18 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Dibujos;
 import java.awt.Graphics;
 
 /**
- * Dependency line for UML
- * @author nwroot
+ * "dibujar" una flecha punteada, seleccionar y arrastras a la pizarra
+ * @param x1 entero que indica posicion inicial en eje x
+ * @param x2 entero que indica posicion final en eje x
+ * @param y1 entero que indica posicion inicial eje y 
+ * @param y2 entero que indica posicion inicial eje y 
+ * @param color indica color seleccionado
  */
 public class Flechapunt extends DrawableLine {
 
     /**
-     * Paint to the screen
+     * dibujar en la pizarra
      * @param g
      */
     @Override

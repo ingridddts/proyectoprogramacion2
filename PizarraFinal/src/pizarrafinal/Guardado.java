8d698b6022b04b7fcb7e305f7460cc516cc7666b
Ingrid <131918653+ingridddts@users.noreply.github.com>
2023-06-30 23:47:24 -0400

    Update Guardado.java
    actualizacion guardar copia

--- PizarraFinal/src/pizarrafinal/Guardado.java
+++ PizarraFinal/src/pizarrafinal/Guardado.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pizarrafinal;
 
-/**
- *
- * @author pepen
- */
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot; //obtener una imagen de la pantalla
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+
+ /**
+     * Guarda una copia de la pizarra
+     * @param pizarra contiene los datos del panel actual
+     */
 public class Guardado {
-    
+    private JPanel tab;
+   
+    public Guardado(JPanel pizarra) {
+        tab = pizarra;
+    }
+    /**
+     * crea ana BufferedImage
+     * @param panel 
+     * @return imagen del panel
+     * @throws AWTException
+     */
+    public static BufferedImage capturarPantalla(JPanel panel) throws AWTException {
+        Point p = panel.getLocationOnScreen();
+        Dimension dim = panel.getSize();
+        Rectangle rect = new Rectangle(p, dim);
+
+        Robot robot = new Robot();
+        return robot.createScreenCapture(rect);
+    }
+
+    /**
+     * Guarda una BufferedImage (captura de pantalla)
+     * @param image alamacena la captura de pantalla
+     * @throws IOException
+     */
+    public void save(BufferedImage image) throws IOException {
+        File outputfile = new File("image.png");
+        ImageIO.write(image, "png", outputfile);
+    }
 }

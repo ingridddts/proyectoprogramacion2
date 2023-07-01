32c9634f7156f8d2d0ceebd0062509542085b4ad
Ingrid <131918653+ingridddts@users.noreply.github.com>
2023-06-30 23:57:35 -0400

    Update DText.java
    agregar texto

--- PizarraFinal/src/Controles/DText.java
+++ PizarraFinal/src/Controles/DText.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controles;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import pizarrafinal.PizarraContainer;
 
 /**
  *
  * @author pepen
  */
-public class DText {
+public class DText extends JFrame{
+    private String contenido;
+    private boolean close = false;
+    public DText(PizarraContainer cont){
+        this.setTitle("Inserte Texto");
+        this.setPreferredSize(new Dimension(300, 100));
+        this.setLayout(new BorderLayout());
+        this.setLocationRelativeTo(null);
+        
+        JButton submit = new JButton("Añadir Cuadro de Texto");
+        submit.setSize(50, 150);
+        this.add(submit, BorderLayout.SOUTH);
+        
+        JTextField namer = new JTextField();
+        namer.setSize(400, 300);
+        this.add(namer, BorderLayout.CENTER);
+        this.pack();
+        
+        submit.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                 contenido = namer.getText();
+                 int index = cont.getTextos().getIndex();
+                 int x = evt.getX();
+                 int y = evt.getY();
+                 cont.getTextos().addText(contenido, x, y, index);
+                 close = true;
+                 dispose();
+            }
+        });   
+        this.setVisible(true);  
+    }
     
+    public String getText(){
+        return contenido;
+    }
 }
